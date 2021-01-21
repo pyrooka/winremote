c0a8409a5d9a59542a1769a741a90101832a7c2a
B <pyrooka@users.noreply.github.com>
2021-01-21 17:05:40 +0100

    First implementation of the windows commands.

--- windows.go
+++ windows.go
@@ -1,17 +1,31 @@
 package main
 
-func lock() {
+import (
+	"errors"
+	"os/exec"
+)
 
+func lock() error {
+	_, err := exec.Command("rundll32.exe", "user32.dll,LockWorkStation").Output()
+	return err
 }
 
-func sleep() {
-
+func sleep() error {
+	return errors.New("sleep is not implemented yet")
 }
 
-func hibernate() {
-
+func hibernate() error {
+	_, err := exec.Command("rundll32.exe", "powrprof.dll,SetSuspendState", "0,1,0").Output()
+	return err
 }
 
-func turnOff() {
+func restart() error {
+	_, err := exec.Command("shutdown", "/r").Output()
+	return err
+}
 
+func turnOff() error {
+	// Using the /f switch to force close all the running application.
+	_, err := exec.Command("shutdown", "/s", "/f").Output()
+	return err
 }
