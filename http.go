7a73642c3fe0ccad6a845b4616a79e0c9d1aa469
B <pyrooka@users.noreply.github.com>
2021-01-21 20:59:22 +0100

    Basic implementation of the HTTP server.

--- http.go
+++ http.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func lockHandler(w http.ResponseWriter, r *http.Request) {
+	err := lock()
+	if err != nil {
+		elog.Error(1, fmt.Sprintf("failed to lock the computer: %v", err))
+	}
+}
+
+func sleepOrHibernateHandler(w http.ResponseWriter, r *http.Request) {
+	err := sleepOrHibernate()
+	if err != nil {
+		elog.Error(1, fmt.Sprintf("failed to lock the computer: %v", err))
+	}
+}
+
+func shutdownHandler(w http.ResponseWriter, r *http.Request) {
+	err := shutdown()
+	if err != nil {
+		elog.Error(1, fmt.Sprintf("failed to lock the computer: %v", err))
+	}
+}
+
+func restartHandler(w http.ResponseWriter, r *http.Request) {
+	err := restart()
+	if err != nil {
+		elog.Error(1, fmt.Sprintf("failed to lock the computer: %v", err))
+	}
+}
+
+func listen() error {
+	http.HandleFunc("/lock", lockHandler)
+	http.HandleFunc("/sleep", sleepOrHibernateHandler)
+	http.HandleFunc("/shutdown", shutdownHandler)
+	http.HandleFunc("/restart", restartHandler)
+
+	return http.ListenAndServe(":8899", nil)
+}
