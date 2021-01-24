590a4628259c60d94e956be6899b4ec7092c9354
B <pyrooka@users.noreply.github.com>
2021-01-24 20:50:01 +0100

    Fix messages.

--- http.go
+++ http.go
@@ -8,21 +8,21 @@ import (
 func sleepOrHibernateHandler(w http.ResponseWriter, r *http.Request) {
 	err := sleepOrHibernate()
 	if err != nil {
-		elog.Error(1, fmt.Sprintf("failed to lock the computer: %v", err))
+		elog.Error(1, fmt.Sprintf("failed to sleep/hibernate the computer: %v", err))
 	}
 }
 
 func shutdownHandler(w http.ResponseWriter, r *http.Request) {
 	err := shutdown()
 	if err != nil {
-		elog.Error(1, fmt.Sprintf("failed to lock the computer: %v", err))
+		elog.Error(1, fmt.Sprintf("failed to shutdown the computer: %v", err))
 	}
 }
 
 func restartHandler(w http.ResponseWriter, r *http.Request) {
 	err := restart()
 	if err != nil {
-		elog.Error(1, fmt.Sprintf("failed to lock the computer: %v", err))
+		elog.Error(1, fmt.Sprintf("failed to restart the computer: %v", err))
 	}
 }
 
