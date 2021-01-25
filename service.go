8dbf48e0d319db6ef90c68e80432e8d22e907db2
B <pyrooka@users.noreply.github.com>
2021-01-25 21:01:52 +0100

    Fix service description.

--- service.go
+++ service.go
@@ -63,7 +63,7 @@ func installService(name, displayName string) error {
 	svcConfig := mgr.Config{
 		DisplayName: displayName,
 		StartType:   mgr.StartAutomatic,
-		Description: "Turn off/sleep/hibernate your computer remotely via HTTP.",
+		Description: "Turn off/restart/sleep/hibernate your computer remotely via HTTP.",
 	}
 
 	s, err = m.CreateService(name, exepath, svcConfig, "is", "auto-started")
